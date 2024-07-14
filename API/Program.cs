a69cf680adf303d1b49434c2dad436634de61b4c
vijay babar <babarvm@gmail.com>
2024-07-14 19:04:37 +0530

    API project setup

---
+++ API/Program.cs
@@ -0,0 +1,18 @@
+// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+// Configure the HTTP request pipeline.
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
